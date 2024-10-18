bb6aaacbef0c8d3e8fe6cba890767a0669022513
veernala <dhanunjayaroa@gmail.com>
2024-10-18 16:24:41 +0530

    as

--- javaseript/weather.js
+++ javaseript/weather.js
@@ -16,6 +16,32 @@ document.getElementById('searchForm').addEventListener('submit', event=>{
 });
 
 
-function fetchWeather(city)
+function fetchWeather(city) {
+  const url = '${BASE_URL}?q=${city}&appid=${API_KEY}&units=metric';
+
+  return fetch(url)
+    .then(response => response.json())
+    .then(data => {
+      if (data.cod === 200) {
+        clearError();
+        return data;
+      } else {
+        throw new Error(data.message);
+      }
+    })
+    .catch(error => {
+      displayError('City not found. Please enter a valid city name.');
+      console.error('Error fetching weather data:', error);
+    });
+}
+
+
+
+fuction displayWeather(data)
+{
+  const weatherDisplay=document.getElementById('weatherDisplay');
+  
+}
+
 
 
